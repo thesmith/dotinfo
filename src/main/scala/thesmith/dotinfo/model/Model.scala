d3c7c64e015e0013a7ccd249c47319a776228aeb
Ben Smith <ben@thesmith.co.uk>
2009-11-29 14:24:07 +0000

    setup jpa with hibernate

--- src/main/scala/thesmith/dotinfo/model/Model.scala
+++ src/main/scala/thesmith/dotinfo/model/Model.scala
@@ -2,10 +2,6 @@ package thesmith.dotinfo.model
 
 import org.scala_libs.jpa.LocalEMF
 import net.liftweb.jpa.RequestVarEM
-import scala.collection.jcl.{BufferWrapper,SetWrapper}
 
-object Model extends LocalEMF("jpaweb") with RequestVarEM {
-  implicit def setToWrapper[A](set : java.util.Set[A]) = new SetWrapper[A]{override def underlying = set}
-  implicit def listToWrapper[A](list : java.util.List[A]) = new BufferWrapper[A]{override def underlying = list}
-}  
+object Model extends LocalEMF("jpaweb") with RequestVarEM
 
